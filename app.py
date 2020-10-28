5e85a6152b703c4bc90cb074d11cfd7a46416ee0
avivwachman <68813905+avivwachman@users.noreply.github.com>
2020-10-28 13:42:43 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import csv
 import json
 from config import FinhubAPI
 from flask import Flask, render_template
@@ -19,66 +18,46 @@ def human_format(num):
 
 
 def init_data():
-    """ Api call for the COVID-19 info and returns a list of dictionaries """
-    url = 'https://finnhub.io/api/v1/covid19/us?'
-    r = requests.get(url, params={'token': FinhubAPI})
+    """ Api call for the COVID-19 info """
     not_state_lst = ['Veteran Affair', 'Federal Bureau of Prisons', 'US Military', 'Northern Mariana Islands',
                      'Wuhan Evacuee', 'US Virgin Islands', 'Diamond Princess', 'Guam', 'Grand Princess',
                      'Navajo Nation', 'Puerto Rico', 'Washington, D.C.']
+    url = 'https://finnhub.io/api/v1/covid19/us?'
+    r = requests.get(url, params={'token': FinhubAPI})
     states_dict = json.loads(r.text)
-    states_lst = list()
-    csv_lst = list()
-    end_data = {'min_cases': states_dict[0]["case"], 'min_cases_name': states_dict[0]["state"],
-                'min_deaths': states_dict[0]['death'], 'min_deaths_name': states_dict[0]["state"], 'most_cases': -1,
-                'most_cases_name': 'ERROR', 'most_deaths': -1, 'most_deaths_name': 'ERROR'}
-    total_lst = [0, 0]
-    for state in states_dict:
-        if state["state"] not in not_state_lst:
-            state_name = state["state"]
-            state_cases = state["case"]
-            state_deaths = state["death"]
-            total_lst[0] = total_lst[0] + state_deaths
-            total_lst[1] = total_lst[1] + state_cases
-            states_lst.append((state_name, human_format(state_cases), human_format(state_deaths)))
-            csv_lst.append({'State': state_name, 'Cases': state_cases})
-            if state_cases < end_data['min_cases']:
-                end_data['min_cases'] = state_cases
-                end_data['min_cases_name'] = state_name
-            elif state_cases > end_data['most_cases']:
-                end_data['most_cases'] = state_cases
-                end_data['most_cases_name'] = state_name
-            if state_deaths < end_data['min_deaths']:
-                end_data['min_deaths'] = state_deaths
-                end_data['min_deaths_name'] = state_name
-            elif state_deaths > end_data['most_deaths']:
-                end_data['most_deaths'] = state_deaths
-                end_data['most_deaths_name'] = state_name
+    states_lst = [(x['state'], human_format(x['case']), human_format(x['death'])) for x in states_dict if
+                  x['state'] not in not_state_lst]
+    csv_lst = [{'State': x['state'], 'Cases': x['case']} for x in states_dict if x['state'] not in not_state_lst]
+    csv_lst = sorted(csv_lst, key=lambda k: k['State'])
     states_lst.sort()
-    total_lst_format = list()
-    end_data_format = dict()
-    for i in total_lst:
-        total_lst_format.append(human_format(i))
+    cases_lst = [(x['case'], x['state']) for x in states_dict if x['state'] not in not_state_lst]
+    death_lst = [(x['death'], x['state']) for x in states_dict if x['state'] not in not_state_lst]
+    end_data = {'min_cases': min(cases_lst)[0], 'min_cases_name': min(cases_lst)[1],
+                'min_deaths': min(death_lst)[0], 'min_deaths_name': min(death_lst)[1], 'most_cases': max(cases_lst)[0],
+                'most_cases_name': max(cases_lst)[1], 'most_deaths': max(death_lst)[0],
+                'most_deaths_name': max(death_lst)[1]}
+    death_only_lst = [x[0] for x in death_lst]
+    case_only_lst = [x[0] for x in cases_lst]
+    total_lst = [human_format(sum(death_only_lst)), human_format(sum(case_only_lst))]
     for end in end_data:
         if isinstance(end_data[end], int):
             end_data[end] = human_format(end_data[end])
-    csv_columns = ['State', 'Cases']
-    csv_file = "csvs/states.csv"
-    try:
-        with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-            writer.writeheader()
-            for data in csv_lst:
-                writer.writerow(data)
-    except IOError:
-        print("I/O error")
-    return states_lst, total_lst_format, end_data
+    return states_lst, total_lst, end_data, csv_lst
 
+
+# change to have better interchange to html
+# combine state list and csv to one by adding human format later
+# total to end data
+# change colors by average
+#add explain comments
+#make chart responsive
 
 @app.route('/index')
 @app.route('/')
 def hello_world():
-    states_lst, total_lst_format, end_data = init_data()
-    return render_template('index.html', states_lst=states_lst, total_lst_format=total_lst_format, end_data=end_data)
+    states_lst, total_lst_format, end_data, csv_lst = init_data()
+    return render_template('index.html', states_lst=states_lst, total_lst_format=total_lst_format, end_data=end_data,
+                           csv_lst=csv_lst)
 
 
 if __name__ == '__main__':
